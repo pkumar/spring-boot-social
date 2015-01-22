eb66a6187707965ac1b0095f343ca3701f61502a
Pradeep Kumar <Pradeep.Kumar@neustar.biz>
2015-01-22 19:08:24 +0530

    Cleanup and gitignore

--- src/main/java/com/pktr/social/controller/TweetsController.java
+++ src/main/java/com/pktr/social/controller/TweetsController.java
@@ -1,6 +1,5 @@
 package com.pktr.social.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +10,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.pktr.social.model.Tweet;
 import com.pktr.social.model.TwitterModel;
 
 @Controller
 public class TweetsController {
+
+  private static final String PKTR_HANDLE = "pradeepktr84";
 
   @Autowired
   private Twitter twitter;
@@ -23,31 +23,18 @@ public class TweetsController {
   @RequestMapping("/")
   public String index(Model model) {
     TwitterModel twitterModel = new TwitterModel();
-    twitterModel.setHandle("pradeepktr84");
+    twitterModel.setHandle(PKTR_HANDLE);
     model.addAttribute("twitterModel", twitterModel);
     return "hello";
   }
-  
-  @RequestMapping(value = "/favorites", method=RequestMethod.POST)
-  public String retrieveFavorites(@ModelAttribute(value="twitterModel") TwitterModel twitterModel, Model model) {
-    List<org.springframework.social.twitter.api.Tweet> tweets =  twitter.timelineOperations().getFavorites(twitterModel.getHandle(), 50);
-    model.addAttribute("tweetList", transformTweets(tweets));
-    model.addAttribute("mytweet", new Tweet());
+
+  @RequestMapping(value = "/favorites", method = RequestMethod.POST)
+  public String retrieveFavorites(
+      @ModelAttribute(value = "twitterModel") TwitterModel twitterModel, Model model) {
+    List<org.springframework.social.twitter.api.Tweet> tweets = twitter.timelineOperations()
+        .getFavorites(twitterModel.getHandle(), 50);
+    model.addAttribute("tweetList", tweets);
     return "favorites";
   }
-  
-  private List<Tweet> transformTweets(List<org.springframework.social.twitter.api.Tweet> tweets) {
-    List<Tweet> tweetList = new ArrayList<Tweet>(tweets.size());
-    for (org.springframework.social.twitter.api.Tweet tweet : tweets) {
-      Tweet mytweet = new Tweet();
-      mytweet.setFromUser(tweet.getFromUser());
-      mytweet.setText(tweet.getText());
-      mytweet.setProfileImageURL(tweet.getProfileImageUrl());
-      tweetList.add(mytweet);
-    }
-    return tweetList;
-  }
-
-
 
 }
